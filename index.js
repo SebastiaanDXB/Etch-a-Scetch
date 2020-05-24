c5ccef56fd3dd52ceac0b3788df2e8c9e692e010
Sebastiaan Jooste <32298404+SebastiaanDXB@users.noreply.github.com>
2020-05-24 18:57:29 +0200

    updated JS

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
+
+let main = document.getElementById("main");
+let bgColor = document.getElementById("bgcolor")
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+function getGridSize() {
+    let userSize = prompt("Please enter a grid size between 16 and 128");
+    return makeRows(userSize,userSize)
+};
+
+function backgroundColor() {
+    document.getElementById("main").style.backgroundColor = bgColor.value;
+};
+
+function pixelColor() {
+    document.getElementById("grid-item").style.backgroundColor = cellColor.value;
+}
+
+function clearGrid() {
+    document.getElementById("container").innerHTML = " "
+};
+
 function makeRows(rows, cols) {
     clearGrid();
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   
+  
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
     
-    
     cell.addEventListener("mouseover", (e) => {
-    color20()
-    color40()
-    color60()
-    color80()
-    color100()
+
+    color20();
+    color40();
+    color60();
+    color80();
+    color100();
 
 
 
@@ -60,12 +85,3 @@ function makeRows(rows, cols) {
 
 makeRows(16, 16);
 
-
-
-
-
-/////////////////////////
-
-do {
-    
-} while (cell.style.backgroundColor != "rgba(0,0,0,${shade[getId]})");
